35a8bf1aea05d8978903d561481359451033f28a
huypd <huygoku37@gmail.com>
2024-11-29 13:42:04 +0700

    Initial commit

---
+++ BookStore/LibraryCore/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using LibraryCore.Models;
+
+namespace LibraryCore.Repositories
+{
+    public interface IOrderRepository:IGenericRepository<Order>
+    {
+        public IList<Order> GetOrderByUserId(int userId);
+        public int[] CountOrdersByMonth();
+        public int[] CountOrdersByDay();
+
+
+        public IList<Order> GetAllOrdersNav();
+    }
+}
---
+++ BookStore/LibraryCore/Repositories/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿using LibraryCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryCore.Repositories
+{
+    public class OrderRepository:GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(LibraryDBContext context) : base(context)
+        {
+            
+        }
+
+        public IList<Order> GetOrderByUserId(int userId)
+        {
+            return dbSet.Include(x=>x.OrderDetails).ThenInclude(od=>od.Book).ThenInclude(od=>od.Category).Where(x=>x.UserId==userId).ToList();
+        }
+
+        public int[] CountOrdersByMonth()
+        {
+            var result = context.Orders
+                .GroupBy(l => l.OrderDate.Month)
+                .OrderBy(g => g.Key)
+                .Select(g => g.Count())
+                .ToArray();
+
+            return result;
+        }
+
+        public IList<Order> GetAllOrdersNav()
+        {
+            return dbSet.Include(x=>x.User).Include(x => x.OrderDetails).ThenInclude(od => od.Book).ThenInclude(od=>od.Category).ToList();
+        }
+
+        public int[] CountOrdersByDay()
+        {
+            var result = context.Orders
+                .GroupBy(o => o.OrderDate.Date) // Nhóm theo ngày
+                .OrderBy(g => g.Key) // Sắp xếp theo ngày
+                .Select(g => g.Count()) // Đếm số lượng đơn hàng trong mỗi nhóm
+                .ToArray(); // Chuyển đổi kết quả thành mảng
+
+            return result;
+        }
+
+    }
+}
